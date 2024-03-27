7585744d8cdbb16a7ff4214216ecf70056ae7d6f
xuwang0803 <thomas940803@gmail.com>
2024-03-27 09:44:26 +0800

    week01

--- week03-1.cpp
+++
@@ -1,7 +0,0 @@
-# include <stdio.h>
-int main()
-{ 
-    int a,b;
-    scanf("%d %d",&a,&b); //用課本講解
-    printf("%d",a+b);
-}
---
+++ week03/week03-1.cpp
@@ -0,0 +1,7 @@
+# include <stdio.h>
+int main()
+{ 
+    int a,b;
+    scanf("%d %d",&a,&b); //用課本講解
+    printf("%d",a+b);
+}
