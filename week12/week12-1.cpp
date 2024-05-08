91f838fe7822701fef1ef5f152e786c458bf6bef
xuwang0803 <144758478+xuwang0803@users.noreply.github.com>
2024-05-08 11:56:43 +0800

    week12

---
+++ week12/week12-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int isPrime (int n)
+    {
+    for(int i=2;i<n;i++)
+        {
+        if(n%i==0)return 0;
+        }
+    }
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(isPrime(n)) printf("n是質數");
+    else printf("n不是質數");
+}
